b3c01e65b2d278c0f7dcc34c5b8a8980e372d679
samuel angel palacio mesa <samuel.palacio@upb.edu.co>
2021-09-25 09:55:20 -0500

    Add TextMeshPro

---
+++ Assets/ButtonReader.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(fileName ="ButtonReader", menuName = "Game/ButtonReader")]
+public class ButtonReader : ScriptableObject
+{
+    
+}
