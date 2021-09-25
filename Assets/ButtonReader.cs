a975e989b9cb884ef8d7a4e7c2092b18633608ef
samuel angel palacio mesa <samuel.palacio@upb.edu.co>
2021-09-25 10:22:41 -0500

    Chanel

--- Assets/ButtonReader.cs
+++ Assets/ButtonReader.cs
@@ -6,5 +6,21 @@ using UnityEngine.Events;
 [CreateAssetMenu(fileName ="ButtonReader", menuName = "Game/ButtonReader")]
 public class ButtonReader : ScriptableObject
 {
-    
+    public event UnityAction redEvent;
+    public event UnityAction yellowEvent;
+    public event UnityAction greenEvent;
+
+    public void OnRed()
+    {
+        redEvent?.Invoke();
+    }
+   
+    public void OnYellow()
+    {
+        yellowEvent?.Invoke();
+    }
+    public void OnGreen()
+    {
+        greenEvent?.Invoke();
+    }
 }
