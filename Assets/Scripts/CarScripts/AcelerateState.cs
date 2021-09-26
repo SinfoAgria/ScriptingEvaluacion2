4d61822285e8394d74546d5df07bf7b614e6029a
samuel angel palacio mesa <samuel.palacio@upb.edu.co>
2021-09-25 22:21:50 -0500

    Organizar la escena

--- Assets/Scripts/CarScripts/AcelerateState.cs
+++ Assets/Scripts/CarScripts/AcelerateState.cs
@@ -2,10 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AcelerateState : ICarState
+public class AcelerateState : ICarState, ICarSmoke
 {
     public void Execute(Car car)
     {
         car.Speed = 10f;
+        ThrowSmoke();
+    }
+
+    public void ThrowSmoke()
+    {
+        Debug.Log("---SMOKE---SMOKE");
     }
 }
